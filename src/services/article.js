4a20eec01b7dcf300bd8e69a89b942bc128fd9c3
Luis Herrera <luis378962@gmail.com>
2023-04-22 18:54:17 -0600

    Final version

--- src/services/article.js
+++ src/services/article.js
@@ -6,16 +6,18 @@ export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders: (headers, { getState }) => {
+        prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
-        }
+        },
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => 'test'
-        })
-    })
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
